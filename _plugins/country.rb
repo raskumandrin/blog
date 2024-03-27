d427358d8e40a95c7ae146147fc8014e743ab934
Stas Raskumandrin <raskumandrin@gmail.com>
2024-03-27 16:24:45 +0100

    beta remove ru categories

---
+++ _plugins/country.rb
@@ -0,0 +1,26 @@
+require 'csv'
+
+module Jekyll
+  class CountryGenerator < Generator
+    def generate(site)
+      generate_pages_from_csv(site, 'countries.csv', 'country')
+      generate_pages_from_csv(site, 'categories.csv', 'category')
+      generate_pages_from_csv(site, 'tags.csv', 'tag')
+    end
+ 
+    def generate_pages_from_csv(site, csv_file, data_key)
+      csv_data = CSV.read("_data/ru/#{csv_file}", headers: true)
+
+      csv_data.each do |row|
+        label = row['label']
+        
+        new_page = Page.new(site, site.source, "", "#{label}.html")
+        new_page.data['layout'] = 'category'
+        new_page.data[data_key] = label
+        new_page.data['head'] = row['title']
+
+        site.pages << new_page
+      end
+    end
+  end
+end
